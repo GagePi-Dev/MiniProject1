4d188a2dc6345eea27b59f7f402e4ab3b78fdae2
Gage Giffin <gage@gagepi.com>
2026-09-05 10:43:23 -0500

    Requirement One
    Added initial comments and section headers.
    HUMAN MADE.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import os
+# INF601 - Advanced Programming in Python
+# Your Name
+# Mini Project 1
 
+# Imports
+import os
 import requests
 from dotenv import load_dotenv
 
+# Load API Token
 load_dotenv()  # reads FHSU_API_TOKEN from the .env file
 
+# Variables
 BASE = "https://practice.fhsucyber.com"
 TOKEN = os.environ.get("FHSU_API_TOKEN")
 headers = {"Authorization": f"Bearer {TOKEN}"}
