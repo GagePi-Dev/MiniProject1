0b6c99255cedb696919389f8c5befb91c387cc9a
Gage Giffin <gage@gagepi.com>
2026-09-05 11:09:40 -0500

    First API Requests + Class Fixes
    Development of first few API requests to list posts and create one. Based off of API documentation examples.
    Also fixed variable names which are now under a class group.
    HUMAN MADE.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Your Name
 # Mini Project 1
 
-# Imports
+# Imports (Requirement #2)
 import os
 import requests
 from dotenv import load_dotenv
@@ -10,17 +10,40 @@ from dotenv import load_dotenv
 # Load API Token
 load_dotenv()  # reads FHSU_API_TOKEN from the .env file
 
-# Variables
-BASE = "https://practice.fhsucyber.com"
-TOKEN = os.environ.get("FHSU_API_TOKEN")
-headers = {"Authorization": f"Bearer {TOKEN}"}
+# API Header (Requirement #3)
+class PracticeHubClient:
+    ENDPOINT = "https://practice.fhsucyber.com"
+    TOKEN = os.environ.get("FHSU_API_TOKEN")
+    HEADERS = {"Authorization": f"Bearer {TOKEN}"}
+
+# List Posts
+posts = requests.get(
+    f"{PracticeHubClient.ENDPOINT}/api/v1/posts",
+    headers=PracticeHubClient.HEADERS,
+    ).json()
+
+my_posts = requests.get(
+    f"{PracticeHubClient.ENDPOINT}/api/v1/posts",
+    headers=PracticeHubClient.HEADERS,
+    params={"mine": True},
+    ).json()
+
+# Create A Post (Requirement #4)
+new_post = requests.post(f"{PracticeHubClient.ENDPOINT}/api/v1/posts", headers=PracticeHubClient.HEADERS, json={
+    "title": "My Post",
+    "body": "Hello world",
+    "tags": ["python"],
+    }).json()
+post_id = new_post["id"]
+
+print(posts)
 
 # Who am I?
-print(requests.get(f"{BASE}/api/v1/me", headers=headers).json())
+#print(requests.get(f"{BASE}/api/v1/me", headers=headers).json())
 
 # Read recent posts
-print(requests.get(f"{BASE}/api/v1/posts", headers=headers).json())
+#print(requests.get(f"{BASE}/api/v1/posts", headers=headers).json())
 
 # Generate some practice data (no public API needed)
-print(requests.get(f"{BASE}/api/v1/datasets/people",
-                   headers=headers, params={"count": 5}).json())
+#print(requests.get(f"{BASE}/api/v1/datasets/people",
+#                   headers=headers, params={"count": 5}).json())
